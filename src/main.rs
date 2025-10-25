53ffd21da8f2b039813b4ec54502ddfbc9c2ff1f
stephen kurtz <stephenjkurtz@gmail.com>
2025-10-25 15:53:52 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+/*
+X - Create a new `about_me` project with the `cargo new` command.
+
+X - Using the `println!` macro, output 3 sentences about yourself.
+Feel free to invoke the macro multiple times.
+
+X - From the Terminal, compile the `main.rs` file inside the `src`
+folder with the Rust compiler, then manually run the executable.
+
+X - From the Terminal, compile the project with the Cargo tool, then
+manually run the executable.
+
+X - From the Terminal, compile and run the project with a single
+Cargo command.
+
+X - Check your program for errors with `cargo check`.
+
+X - Add a comment at the top of your source code explaining how to
+compile the program for new Rust developers.
+
+Add some spaces and line breaks to the code so that it is formatted
+in an ugly manner. From the Terminal, style the code with the
+`cargo fmt` command.
+
+Replace the `println!` macro with `print!`. What happens?
+*/
+
+
+// To run the program run the about_me.exe file
 fn main() {
-    println!();
-    println!();
-    println!();
-    println!();
-    println!();
-    println!();
-    println!();
-    println!("Hello, world!");
+    print!("Hi, I'm Steve, a software developer.");
+    print!("I live and work in Philadelphia.");
+    print!("My favorite current programming language is Rust >:)");
 }
