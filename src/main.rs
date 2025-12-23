91f03fd196f8161e7e6f83c24fee8ae8e744e109
Stephen Kurtz <stephenjkurtz@gmail.com>
2025-12-23 15:07:48 -0500

    complete section 2 project

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-#![allow(unused_variables)] //applies to the whole file because of the exclamation point and being places at the top line
-type Meters = i32;
+mod instructions;
+const TOUCHDOWN_POINTS: i32 = 6;
 
-//#[allow(unused_variables)] applies to the whole fn
 fn main() {
-    //#[allow(unused_variables)] only applies to the following line of code
-    let mile_race_meters: Meters = 1600;
-    let two_mile_race_meters: Meters = 3200;
+    let season: &str = "Fall";
+    #[allow(warnings)]
+    let mut points_scored: i32 = 32;
+    points_scored = 35;
+
+    let _event_time: String = "06:00".to_string();
+    let event_time: i32 = 6;
+
+    #[allow(unused_variables)]
+    let favorite_beverage = "coffee";
+
+    println!("My favorite season is {0}. The team scored {1} points. A touchdown is worth {2} points. event time is at {3}", season, points_scored, TOUCHDOWN_POINTS, event_time);
+    
 }
-
-
-/*
-COMPILER DIRECTIVES ARE ANNOTATIONS WE ADD TO OUR CODE THAT TELLS COMPILER HOW TO THE PARSE ARE FILE.
-
-IN THIS SECTION WE USE THE BASIC ALLOW DIRECTIVE BUT WILL COVER MORE IN FOLLOWING CHAPTERS - THEY ALL FOLLOW THE SAME PATTERN.
-
-THE BOOK CALLS THESE "ATTRIBUTES" RATHER THAN COMPILER DIRECTIVES.
- */
