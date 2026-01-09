dce84329b7b1bf87cc75fdeea411d05cf9a2dd55
Stephen Kurtz <stephenjkurtz@gmail.com>
2026-01-09 16:30:38 -0500

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 
 fn main() {
+    // 8 bits = 1 byte of memory
     let eight_bit_signed: i8 = -128;
     let eight_bit_unsigned: u8 = 255;
 
+    // 16 bits = 2 bytes of memory
     let sixteen_bit_signed: i16 = -32768;
     let sixteen_bit_unsigned: u16 = 65535;
 
+    // 32 bits = 4 bytes of memory
     let thirty_two_bit_signed: i32 = -2147483648;
     let thirty_two_bit_unsigned: u32 = 4294967295;
+
+    // 1024 bytes = 1 kilobyte
+    // 1024 kilobytes = 1 megabyte
+    // 1024 megabytes = 1 gigabyte
+    // 1024 gigabytes = 1 terabyte
 }
